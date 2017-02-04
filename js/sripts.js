b986aef887f61a310cc61e7eb76573f1e76a1acf
plotkopavlo <plotkopavlo@gmail.com>
2017-02-04 18:12:16 +0200

    Add files via upload

---
+++ js/sripts.js
@@ -0,0 +1,88 @@
+
+
+$(document).ready(function(){
+ $('.team--list').slick({
+  dots: true,
+  infinite: true,
+  speed: 300,
+  prevArrow: "",
+  nextArrow: "",
+  slidesToShow: 4,
+  slidesToScroll: 4,
+  responsive: [
+    {
+      breakpoint: 1024,
+      settings: {
+        slidesToShow: 3,
+        slidesToScroll: 3,
+        infinite: true,
+        dots: true
+      }
+    },
+    {
+      breakpoint: 600,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 2
+      }
+    },
+    {
+      breakpoint: 480,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1
+      }
+    }
+  ]
+  });
+  $('.team--list .slick-list').addClass("team--sclick-list");
+  
+  $('.clients--list').slick({
+  dots: true,
+  infinite: true,
+  speed: 300,
+  prevArrow: "",
+  nextArrow: "",
+  slidesToShow: 5,
+  slidesToScroll: 5,
+  responsive: [
+    {
+      breakpoint: 1024,
+      settings: {
+        slidesToShow: 3,
+        slidesToScroll: 3,
+        infinite: true,
+        dots: true
+      }
+    },
+    {
+      breakpoint: 600,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 2
+      }
+    },
+    {
+      breakpoint: 480,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1
+      }
+    }
+  ]
+  });
+  $('.clients--list .slick-list').addClass("clients--sclick-list");
+  
+  $('.testimonials--list').slick({
+  dots: true,
+  infinite: true,
+  speed: 300,
+  prevArrow: "",
+  nextArrow: "",
+  slidesToShow: 1,
+  slidesToScroll: 1,
+  
+  });
+  $('.testimonials--list .slick-list').addClass("testimonials--sclick-list ");
+  
+});
